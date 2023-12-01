b10116e92ef4bbf0587e19523fea6f4924c3b7a9
ProfNaama <nami@cmu.edu>
2023-12-01 11:28:41 -0500

    adding toggel

--- JavaScript examples/active nav bar/script.js
+++ JavaScript examples/active nav bar/script.js
@@ -1,6 +1,7 @@
 function activeNavBar() {
     var navLinks= document.querySelectorAll('nav a');
     var curentLocation =  window.location;
+    console.log(curentLocation, navLinks);
     navLinks.forEach(element => {
         if (element.href == curentLocation) {
             console.log("ok");
@@ -10,3 +11,11 @@ function activeNavBar() {
 }
 
 activeNavBar();
+
+function rotateLogo() {
+    var elementToRotate = document.getElementsByClassName("logo");
+    console.log(elementToRotate); 
+    elementToRotate.classList.toggle("rotated90");
+  }
+
+rotateLogo();
